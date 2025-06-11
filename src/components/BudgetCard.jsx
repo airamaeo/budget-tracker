79cb2966ddbeda8f026b8cb273a02025d5202b0c
Aira Mae Ogaco <airamaeogaco@gmail.com>
2025-06-11 20:59:11 +0100

    Add budget calculation logic and integrate expenses with BudgetCard

--- src/components/BudgetCard.jsx
+++ src/components/BudgetCard.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 
-export default function BudgetCard ({name, max}) {
+export default function BudgetCard ({name, max, expenses}) {
+    const total = expenses.reduce(
+        (sum, expense) => sum + expense.amount, 
+        0
+    );
+
+    const remBudget = max - total;
+
     return (
         <div>
             <h3>{name}</h3>
             <p>Max: €{max}</p>
+            <p>Total Expense: €{total}</p>
+            <p>Budget Remaining: €{remBudget}</p>
         </div>
     );
 }
